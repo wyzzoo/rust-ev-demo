b2ba7d7225e7389a698fa39bc4f3cd434463b0ee
wyzzoo <wyzzoo@protonmail.com>
2026-03-11 13:34:35 +0800

    wip: event loop with listener/stream dispatch

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ use polling::{Event, Events, Poller};
 use std::io;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::env;
+
+enum EventSource {
+    Listener(TcpListener),
+    Stream(TcpStream),
+}
 
 struct EventCallback {
     on_read: Option<Box<dyn Fn()>>,
@@ -19,8 +25,11 @@ enum Timer {
 
 struct EventLoop {
     poller: Poller,
-    events: HashMap<u32, EventCallback>,
-    timers: Vec<Timer>
+    events: HashMap<usize, EventCallback>,
+    timers: Vec<Timer>,
+    stop: bool,
+    fds: HashMap<usize, EventSource>,
+    next_key: usize,
 }
 
 impl EventLoop {
@@ -29,16 +38,65 @@ impl EventLoop {
            poller: Poller::new()?,
            events: HashMap::new(),
            timers: Vec::new(),
+           stop: false,
+           fds: HashMap::new(),
+           next_key: 1,
        })
    }
 
-   fn run() {
+   fn listen(self:&mut Self, addr: &str) -> io::Result<()> {
+       let socket = TcpListener::bind(addr)?;
+       socket.set_nonblocking(true)?;
+       let key = self.get_key();
+       self.fds.insert(key, EventSource::Listener(socket.try_clone()?));
+       unsafe{ self.poller.add(&socket, Event::readable(key))? };
+       Ok(())
+   }
+
+   fn get_key(self:&mut Self) -> usize {
+       let key = self.next_key;
+       self.next_key += 1;
+       key
+   }
+
+   fn run(self:&mut Self) -> io::Result<()> {
+       let mut events = Events::new();
+
+       while !self.stop {
+           events.clear();
+           self.poller.wait(&mut events, None)?;
+
+           for ev in events.iter() {
+               match self.fds.get(&ev.key).ok_or(()) {  
+
+                Result::Ok(EventSource::Listener(socket)) => {
+
+                   let (stream, _) = socket.accept()?;
+                           stream.set_nonblocking(true).unwrap();
+                           unsafe{ self.poller.add(&stream, Event::readable(0))? };
+               }
+              _ => break,
+           }
+       }
+       }
+
+       Ok(())
+   }
+
+   fn stop(self:&mut Self) {
+    self.stop = true
    }
 }
 
-fn main() {
+fn on_conn(stream:&mut TcpStream) {
+    println!("on_conn");
+}
+
+fn main() -> std::io::Result<()> {
     println!("=== start ===");
-    let ev = EventLoop::new();
-    ev?.run();
+    let mut ev = EventLoop::new()?;
+    ev.listen("127.0.0.1:8080")?;
+    ev.run();
     println!("=== end ===");
+    Ok(())
 }
