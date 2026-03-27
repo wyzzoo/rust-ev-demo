b1914e5792452c3dee8ed5a6772df1fe550506b9
wyzzoo <wyzzoo@protonmail.com>
2026-03-27 12:56:33 +0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::env;
-
+use std::vec::Vec;
 use std::collections::HashMap;
-use std::vec;
 
 use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
@@ -10,9 +8,28 @@ use polling::{Event, Events, Poller};
 
 use clap::Parser;
 
+struct TcpStreamSource {
+    key: usize,
+    stream: TcpStream,
+    // write_buf: Vec<u8>,
+    read_buf: Vec<u8>,
+}
+
+impl TcpStreamSource {
+    fn new(key: usize, stream: TcpStream) -> TcpStreamSource {
+        TcpStreamSource {
+            key,
+            stream,
+            // write_buf: Vec::new(),
+            read_buf: Vec::new(),
+        }
+    }
+}
+
+
 enum EventSource {
     Listener(TcpListener),
-    Stream(TcpStream),
+    Stream(TcpStreamSource),
 }
 
 struct EventCallback {
@@ -87,23 +104,46 @@ impl EventLoop {
                   let socket = socket.try_clone()?;
           self.on_conn(&socket)?;
       }
-      if let Some(EventSource::Stream(socket)) = self.fds.get(&ev.key) {
-                  let mut socket = socket.try_clone()?;
-          self.on_read(&mut socket)?;
+      if let Some(EventSource::Stream(stream_source)) = self.fds.get(&ev.key) {
+          if ev.writable {
+              self.on_write(ev.key)?;
+          }
+
+          if ev.readable {
+              self.on_read(ev.key)?;
+          }
       }
       Ok(())
     }
 
-    fn on_read(&mut self, socket: &mut TcpStream) -> io::Result<()> {
+    fn on_write(&mut self, key: usize) -> io::Result<()> {
+        if let EventSource::Stream(stream_source) = self.fds.get_mut(&key).unwrap() {
+            let mut socket = &stream_source.stream;
+            socket.write_all(&stream_source.read_buf)?;
+        }
+        Ok(())
+    }
+
+    fn on_read(&mut self, key: usize) -> io::Result<()> {
         let mut buf = [0; 1024];
+        if let EventSource::Stream(stream_source) = self.fds.get_mut(&key).unwrap() {
         loop {
+            let mut socket = &stream_source.stream;
             let n = socket.read(&mut buf)?;
             println!("read {} bytes", n);
+            unsafe { self.poller.modify(&socket, Event::readable(stream_source.key)) };
             if n == 0 {
                 break;
             }
+            stream_source.read_buf.extend_from_slice(&buf[0..n]);
+            unsafe { self.poller.modify(&socket, Event::writable(stream_source.key)) };
+            println!("pending write {} bytes", n);
         }
         Ok(())
+    }
+        else {
+        panic!("on_read: invalid key");
+    }
     }
 
     fn stop(&mut self) {
@@ -114,9 +154,9 @@ impl EventLoop {
         let (stream, _) = socket.accept()?;
         stream.set_nonblocking(true)?;
         let key = self.get_key();
-        self.fds
-            .insert(key, EventSource::Stream(stream.try_clone()?));
         unsafe { self.poller.add(&stream, Event::readable(key))? };
+        self.fds
+            .insert(key, EventSource::Stream(TcpStreamSource::new(key, stream)));
         Ok(())
     }
 }
@@ -127,19 +167,13 @@ struct Command {
     port: u16,
 }
 
-impl Command {
-    fn new() -> Command {
-        Command::parse()
-    }
-}
-
 fn main() -> io::Result<()> {
     println!("=== start ===");
     let mut ev = EventLoop::new()?;
-    let args = Command::new();
+    let args = Command::parse();
     println!("port: {}", args.port);
     ev.listen(&format!("127.0.0.1:{}", args.port))?;
-    ev.run();
+    ev.run()?;
     println!("=== end ===");
     Ok(())
 }
